596736664dc1f546f8f0ca50cdcfbf4a848d4b16
Nikita C <nikita149156@gmail.com>
2021-08-21 17:22:25 +0530

    user_data

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,74 @@
+class Api::UsersController < ApplicationController
+    before_action :getUser, only: [:updateUser, :deleteUser, :showUser]
+    def getUsers
+        user = User.all
+        if user
+            render json: user, status: :ok
+        else
+            render json: { msg: "user Empty" }, status: :unprocessable_entity  
+        end
+    end
+
+    def addUser
+        user = User.new(userparams)
+
+        if user.save()
+            render json: user, status: :ok
+        else
+            render json: {msg: "user not added"}, status: :unprocessable_entity
+        end
+    end
+
+    def showUser
+        if @user
+            render json: @user, status: :ok
+            
+        else
+            render json: { msg: "User not Found"}, status: :unprocessable_entity
+        end
+    end
+
+    def updateUser
+        if @user
+            if@user.update(userparams)
+                render json: @user, status: :ok
+            else
+                render json: { msg: "update Failed" }, status: :unprocessable_entity
+            end
+        else
+            render json: { msg: "User not Found"}, status: :unprocessable_entity
+        end
+    end
+
+    def deleteUser
+        if @user
+            if@user.destroy()
+                render json: { msg: "User deleted" }, status: :ok
+            else
+                render json: { msg: "update Failed" }, status: :unprocessable_entity
+            end
+        else
+            render json: { msg: "User not Found" }, status: :unprocessable_entity
+        end
+    end
+
+    def search 
+        if params[:search].blank?  
+            # render json: { msg: "Empty field!"}, status: :unprocessable_entity 
+            render json: { msg: "Not Found!"}, status: :unprocessable_entity 
+        else  
+            @parameter = params[:search]
+            render json: @parameter, status: :ok
+            @results = User.all.where("lower(firstName) LIKE :search", search: "%#{@parameter}%") 
+        end  
+    end
+    
+    private
+        def userparams
+            params.permit(:firstName, :lastName, :email);
+        end
+        
+        def getUser
+            @user = User.find(params[:id])
+        end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get 'users', action: :getUsers, controller: :users
+    post 'user', action: :addUser, controller: :users
+    get 'show', action: :showUser, controller: :users
+    put 'update', action: :updateUser, controller: :users
+    delete 'delete', action: :deleteUser, controller: :users
+    get 'search', action: :search, controller: :users
+  end
+end
